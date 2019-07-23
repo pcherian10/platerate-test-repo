5273fda70bdf2765a94851f1c23a6670b1dff13e
pcherian10 <pcherian10@gmail.com>
2019-07-23 01:07:54 -0400

    set up ejs and basic routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express();
+require('./routes/testRoutes')(app);
+
 
 app.use(bodyParser.json())
 
--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,6 +1,14 @@
 const axios = require('axios')
+const ejs = require('ejs')
 
 module.exports = app => {
+  app.get('/', (req, res) => {
+    people = ['geddy', 'neil', 'alex'],
+    html = ejs.render('<%= people.join(", "); %>', {people: people});
+
+    res.send(html)
+   })
+
   app.get('/posts', (req, res) => {
     const getPostData = () => {
       return axios.get(`https://jsonplaceholder.typicode.com/posts`)
@@ -13,5 +21,9 @@ module.exports = app => {
   app.get('/aboutme', (req, res) => {
     const { description, tech, techstack, hobbies } = req.params 
     console.log(description)
+    console.log(tech)
+    console.log(techstack)
+    console.log(hobbies)
+
   })
 }

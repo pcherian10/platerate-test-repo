0d684887e77dc36d26d274b3de045623b534d793
pcherian10 <pcherian10@gmail.com>
2019-07-23 14:55:13 -0400

    set up route for data

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -15,17 +15,33 @@ module.exports = app => {
       }
     getPostData().then(posts => {
     //Only the title and body of all the posts should be displayed in an centered and ordered HTML list on the DOM
-      console.log(posts)
       res.render('posts', { posts })
     })
   })
 
   app.get('/aboutme', (req, res) => {
-    const { description, tech, techstack, hobbies } = req.params 
-    console.log(description)
-    console.log(tech)
-    console.log(techstack)
-    console.log(hobbies)
-    res.render('/aboutme')
+    const { q } = req.query
+    const answers = [
+      { question: "Tell me about yourself", answer: "I'm a full stack engineer with a background in finance and photography." },
+      { question: "What excites you about technology?", answer: "The ability to build new and innovative things" },
+      { question: "What is your preferred technology stack?", answer: "PostgreSQL, Node, Express, React" },
+      { question: "what are your favorite hobbies?", answer: "photography and gadget shopping"}
+    ]
+    let data = null;
+
+    switch( q ) {
+      case 'description':
+        data = answers[0]
+      case 'tech':
+        data = answers[1]
+      case 'techstack':
+        data = answers[2]
+      case 'hobbies':
+        data = answers[3]
+      default:
+        data = answers
+    }
+
+    res.render('aboutme', data)
   })
 }
